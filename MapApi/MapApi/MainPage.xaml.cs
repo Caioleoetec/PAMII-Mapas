1df4dc7f13d2e9079e637dd878a6c84e0bc11803
caioLeo <caioleo.etec@gmail.com>
2024-09-18 17:17:02 -0300

    primeiro commit

---
+++ MapApi/MapApi/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Maui.Maps;
+
+namespace MapApi
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+            var location = new Location(-23.545047817230532, -46.474154103611134); //<- Neo Química Arena
+            var mapSpan = new MapSpan(location, 0.01, 0.01);
+            map.MoveToRegion(mapSpan);
+        }
+
+    }
+
+}
